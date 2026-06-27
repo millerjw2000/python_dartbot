70ecebedd66e7bfd2183a3133566b9f34a9cbb6b
millerjw2000 <millerjw2000@gmail.com>
2026-06-27 01:08:23 -0400

    added 404

--- routes.py
+++ routes.py
@@ -9,5 +9,9 @@ def index():
 
     return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=16240)
