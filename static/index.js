f05056253f42d9c1c9e108dbfef5f5c8fe5798a3
millerjw2000 <millerjw2000@gmail.com>
2026-06-29 20:49:02 -0400

    removed 1 line

--- static/index.js
+++ static/index.js
@@ -37,7 +37,6 @@ new_game_submit_button.addEventListener("click", function(event) {
         return
     }
 
-    console.log('dsadsadsadsadsa')
     window.location.href = (`/new_game/difficulty=${selected_diff}`);
 
 });
